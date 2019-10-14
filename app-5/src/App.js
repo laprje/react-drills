56db2790b0e2bb094c5b1b0b342868a21a33027b
L J <laprje@gmail.com>
2019-10-14 16:24:49 -0600

    finished app 5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Image from './Image'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Image url = 'https://www.topgear.com/sites/default/files/styles/16x9_1280w/public/images/news-article/2019/05/9a5164f92224af3873d6615efd91cf1f/288gto-3.jpg?itok=2UVcsF_u' />
       </div>
     );
   }
