9cc889fb511ac9b2f225f1560bda41dc995d02b1
L J <laprje@gmail.com>
2019-10-14 16:48:16 -0600

    made some progress on app 6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -3,17 +3,23 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state ={
+      list: [],
+      input: ''
+    };
+
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  //start here with making the handleAdd function//
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+     <div>
+
+     </div>
+    
     );
   }
 }
