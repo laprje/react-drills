29ad464e57b0d12cd66835e9306f5765f91d49fb
L J <laprje@gmail.com>
2019-10-14 18:24:45 -0600

    finished app 6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,27 +1,50 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+  import React, { Component } from "react";
+  // import logo from "./logo.svg";
+  import "./App.css";
+  import Todo from "./Todo";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state ={
-      list: [],
-      input: ''
-    };
+  export default class App extends Component {
+    constructor() {
+      super();
+      this.state = {
+        list: [],
+        input: ""
+      };
 
-    this.handleAdd = this.handleAdd.bind(this);
+      this.handleAddTask = this.handleAddTask.bind(this);
+    }
+
+    handleInputChange(value) {
+      this.setState({ input: value });
+    }
+
+    handleAddTask() {
+      this.setState({
+        list: [...this.state.list, this.state.input],
+        input: ""
+      });
+    }
+
+    render() {
+      let list = this.state.list.map((element, index) => {
+        return <Todo key={index} task={element} />;
+      });
+
+      return (
+        <div className="App">
+          <h1>To-do list:</h1>
+
+          <div>
+            <input
+            value={this.state.input}
+            placeholder="Enter new task"
+            onChange={e => this.handleInputChange(e.target.value)}/>
+            <button onClick={this.handleAddTask}>Add</button>
+          </div>
+          <br />
+
+          {list}
+        </div>
+      );
+    }
   }
-
-  //start here with making the handleAdd function//
-  render() {
-    return (
-     <div>
-
-     </div>
-    
-    );
-  }
-}
-
-export default App;
